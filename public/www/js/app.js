8687a68674ac51f38607d31757b173eeb8399c5b
xuxl <xuxle@yonyou.com>
2015-10-13 14:52:41 +0800

    amd

--- public/www/js/app.js
+++ public/www/js/app.js
@@ -1,51 +1,54 @@
-var routeApp = angular.module('routeApp', ['ngAnimate', 'ui.router', 'mgcrea.ngStrap', 'useManagerCtrls']);
 
-routeApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/index');
+define(['angular', 'angularAnimate', 'angularUiRouter', 'angularStrap', 'angularStrapTpl', 'js/controllers'], function () {
+    angular.module('routeApp', ['ngAnimate', 'ui.router', 'mgcrea.ngStrap', 'useManagerCtrls'])
 
-    $stateProvider
-        .state('index', {
-            url: '/index',
-            views: {
-                '': {
-                    templateUrl: 'tpls/index.html'
-                },
-                'navBar@index': {
-                    templateUrl: 'tpls/navbar.html',
-                    controller: 'NavBarCtrl'
-                },
-                'mainView@index': {
-                    templateUrl: 'tpls/main.html'
-                }
-            }
-        })
-        .state('index.usemanager', {
-            url: '/usemanager',
-            views: {
-                'mainView@index': {
-                    templateUrl: 'tpls/usemanager.html',
-                    controller: 'useManager'
-                }
-            }
-        })
-        .state('index.usemanager.superuser', {
-            url: '/superuser',
-            templateUrl: 'tpls/superuser.html'
-        })
-        .state('index.usemanager.miduser', {
-            url: '/miduser',
-            templateUrl: 'tpls/miduser.html'
-        })
-        .state('index.usemanager.lowuser', {
-            url: '/lowuser',
-            templateUrl: 'tpls/lowuser.html'
-        })
-        .state('index.usemanager.blackuser', {
-            url: '/blackuser',
-            templateUrl: 'tpls/blackuser.html'
-        })
-        .state('index.usemanager.addusertype', {
-            url: '/addusertype',
-            templateUrl: 'tpls/addusertype.html'
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/index');
+
+            $stateProvider
+                .state('index', {
+                    url: '/index',
+                    views: {
+                        '': {
+                            templateUrl: 'tpls/index.html'
+                        },
+                        'navBar@index': {
+                            templateUrl: 'tpls/navbar.html',
+                            controller: 'NavBarCtrl'
+                        },
+                        'mainView@index': {
+                            templateUrl: 'tpls/main.html'
+                        }
+                    }
+                })
+                .state('index.usemanager', {
+                    url: '/usemanager',
+                    views: {
+                        'mainView@index': {
+                            templateUrl: 'tpls/usemanager.html',
+                            controller: 'useManager'
+                        }
+                    }
+                })
+                .state('index.usemanager.superuser', {
+                    url: '/superuser',
+                    templateUrl: 'tpls/superuser.html'
+                })
+                .state('index.usemanager.miduser', {
+                    url: '/miduser',
+                    templateUrl: 'tpls/miduser.html'
+                })
+                .state('index.usemanager.lowuser', {
+                    url: '/lowuser',
+                    templateUrl: 'tpls/lowuser.html'
+                })
+                .state('index.usemanager.blackuser', {
+                    url: '/blackuser',
+                    templateUrl: 'tpls/blackuser.html'
+                })
+                .state('index.usemanager.addusertype', {
+                    url: '/addusertype',
+                    templateUrl: 'tpls/addusertype.html'
+                });
         });
 });
---
+++ public/www/js/main.js
@@ -0,0 +1,33 @@
+
+requirejs.config({
+    baseUrl: '../www',
+    paths: {
+        app: 'js/app',
+        angular: 'lib/angular/angular',
+        angularAnimate: 'lib/angular-animate/angular-animate',
+        angularUiRouter: 'lib/angular-ui-router/release/angular-ui-router',
+        angularStrap: 'lib/angular-strap/dist/angular-strap',
+        angularStrapTpl: 'lib/angular-strap/dist/angular-strap.tpl'
+    },
+    shim: {
+        angularAnimate: {
+            deps: ['angular']
+        },
+        angularUiRouter: {
+            deps: ['angular']
+        },
+        angularStrap: {
+            deps: ['angular']
+        },
+        angularStrapTpl: {
+            deps: ['angular', 'angularStrap']
+        }
+    }
+});
+
+// Start the main app logic.
+requirejs(['app'], function () {
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, ['routeApp']);
+    });
+});
