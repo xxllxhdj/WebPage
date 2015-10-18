85ecb38a0d3755c9aab77245c9e9560105d6c4d9
xxl <tx082xuxl@126.com>
2015-10-18 13:10:16 +0800

    整理

--- server/admin/controllers/admin.controller.js
+++ server/admin/controllers/admin.controller.js
@@ -4,5 +4,7 @@
  * Render the main application page
  */
 exports.renderIndex = function (req, res) {
+    //console.log('/admin:' + req.query.method);
+    //console.log('/admin:' + JSON.stringify(req.body));
     res.send('Hello World');
 };
--- server/api/controllers/api.controller.js
+++ server/api/controllers/api.controller.js
@@ -4,5 +4,7 @@
  * Render the main application page
  */
 exports.renderIndex = function (req, res) {
+    //console.log('/api:' + req.query.method);
+    //console.log('/api:' + JSON.stringify(req.query));
     res.send('Hello World');
 };
